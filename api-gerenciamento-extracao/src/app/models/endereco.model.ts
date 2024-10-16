0115c761d262d46846fe2713167a464dadb5e348
stefanyGomes-dev <145521642+stefanyGomes-dev@users.noreply.github.com>
2024-10-16 03:21:47 +0000

    add get/set

--- api-gerenciamento-extracao/src/app/models/endereco.model.ts
+++ api-gerenciamento-extracao/src/app/models/endereco.model.ts
@@ -9,7 +9,7 @@ export class Endereco {
     @Column({ type: 'varchar', length: 255 })
     private rua: string;
    
-    @Column({ type: 'int'})
+    @Column({ type: 'int' })
     private numero: number;
    
     @Column({ type: 'varchar', length: 50 })
@@ -23,5 +23,62 @@ export class Endereco {
    
     @Column({ type: 'varchar', length: 8 })
     private cep: string;
-}
 
+    // Getters e Setters
+
+    public getId(): number {
+        return this.id;
+    }
+
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getRua(): string {
+        return this.rua;
+    }
+
+    public setRua(rua: string): void {
+        this.rua = rua;
+    }
+
+    public getNumero(): number {
+        return this.numero;
+    }
+
+    public setNumero(numero: number): void {
+        this.numero = numero;
+    }
+
+    public getBairro(): string {
+        return this.bairro;
+    }
+
+    public setBairro(bairro: string): void {
+        this.bairro = bairro;
+    }
+
+    public getCidade(): string {
+        return this.cidade;
+    }
+
+    public setCidade(cidade: string): void {
+        this.cidade = cidade;
+    }
+
+    public getEstado(): string {
+        return this.estado;
+    }
+
+    public setEstado(estado: string): void {
+        this.estado = estado;
+    }
+
+    public getCep(): string {
+        return this.cep;
+    }
+
+    public setCep(cep: string): void {
+        this.cep = cep;
+    }
+}
--- api-gerenciamento-extracao/src/app/models/imovel.model.ts
+++ api-gerenciamento-extracao/src/app/models/imovel.model.ts
@@ -2,12 +2,12 @@ import { Column, OneToOne, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { Endereco } from "./endereco.model";
 
 @Entity("imovel")
-export class Imovel{
+export class Imovel {
    
     @PrimaryGeneratedColumn()
     private id: number;
 
-    @OneToOne(()=> Endereco, {cascade: true})
+    @OneToOne(() => Endereco, { cascade: true })
     private endereco: Endereco;
    
     @Column({ type: 'varchar', length: 150 })
@@ -16,12 +16,59 @@ export class Imovel{
     @Column({ type: 'int' })
     private areaConstruida: number;
   
-    @Column({ type: 'int'})
+    @Column({ type: 'int' })
     private areaPrivada: number;
    
-    @Column({ type: 'int'})
+    @Column({ type: 'int' })
     private areaComum: number;
 
+    // Getters e Setters
 
+    public getId(): number {
+        return this.id;
+    }
 
+    public setId(id: number): void {
+        this.id = id;
+    }
+
+    public getEndereco(): Endereco {
+        return this.endereco;
+    }
+
+    public setEndereco(endereco: Endereco): void {
+        this.endereco = endereco;
+    }
+
+    public getCaracteristica(): string {
+        return this.caracteristica;
+    }
+
+    public setCaracteristica(caracteristica: string): void {
+        this.caracteristica = caracteristica;
+    }
+
+    public getAreaConstruida(): number {
+        return this.areaConstruida;
+    }
+
+    public setAreaConstruida(areaConstruida: number): void {
+        this.areaConstruida = areaConstruida;
+    }
+
+    public getAreaPrivada(): number {
+        return this.areaPrivada;
+    }
+
+    public setAreaPrivada(areaPrivada: number): void {
+        this.areaPrivada = areaPrivada;
+    }
+
+    public getAreaComum(): number {
+        return this.areaComum;
+    }
+
+    public setAreaComum(areaComum: number): void {
+        this.areaComum = areaComum;
+    }
 }
