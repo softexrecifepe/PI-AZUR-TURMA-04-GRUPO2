5fa464a02b373153c511306ceccb8eaa158c6213
Jandyra <jandyraborgespestananeta@gmail.com>
2024-10-21 23:12:59 -0300

    adicionado controllers e routes - representante

---
+++ api-gerenciamento-extracao/src/app/controllers/representante.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import { RepresentanteService } from "../services/representante.service";
+import { BaseController } from "./base.controller";
+import { CreateRepresentanteRequestDto } from "../dtos/representante/create-representante-request-dto";
+import { UpdateRepresentanteRequestDto } from "../dtos/representante/update-representante-request-dto";
+
+export class RepresentanteController extends BaseController<RepresentanteService> {
+    constructor() {
+        super(new RepresentanteService());
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        const dto = new CreateRepresentanteRequestDto({
+            ...req.body
+        });
+        return this.handleRequest(req, res, next, async () => this.service.create(dto), "Representante criado com sucesso");
+    }
+
+    async update(req: Request, res: Response, next: NextFunction) {
+        const dto = new UpdateRepresentanteRequestDto({
+            ...req.body
+        });
+        const id = req.params.id;
+        return this.handleRequest(req, res, next, async () => this.service.update(id, dto), "Representante atualizado com sucesso");
+    }
+}
