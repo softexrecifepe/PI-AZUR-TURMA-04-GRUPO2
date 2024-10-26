1a31e86a6160927b9e8370fdb8e86d9021964189
Eduardo-J-S <eduardojose59@hotmail.com>
2024-10-25 22:42:32 -0300

    create dtos, validations and service

---
+++ api-gerenciamento-extracao/src/app/dtos/aquisicao-imovel/aquisicaoImovel-response-dto.ts
@@ -0,0 +1,18 @@
+import { Imovel } from "../../models/imovel.model";
+
+export type AquisicaoImovelResponseDto = {
+    id: string;
+    created_at: Date;
+    valorAquisicao: number;
+    recursosProprios: number;
+    recursosFGTS: number;
+    financiamentoCredora: number;
+    origemRecursos: string;
+    normaRegulamentadora: string;
+    valorAcessorias: number;
+    valorDivida: number;
+    valorLeilao: number;
+    sistemaAmortizacao: string;
+    attSaldoDevedor: number;
+    // imovel: Imovel;
+}
